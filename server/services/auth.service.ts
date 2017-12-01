f47ea6b95cc932ec6b78501194cad8bc56bf24e8
Dennis Enselmann <d.enselmann@fh-muenster.de>
2017-12-01 01:45:54 +0100

    Lint auth service

--- server/services/auth.service.ts
+++ server/services/auth.service.ts
@@ -16,12 +16,29 @@ export interface JWTClaimSet {
  * Service to encode, decode JWT and hash, validate passwords
  */
 export const authService = {
+    /**
+     * Hashes a clear password with bcrypt
+     * @param password
+     */
     hashPassword(password: string) {
         return new Promise<string>((resolve, reject) => {
             bcrypt.hash(password, 10, (err, hash) => {
                 err ? reject(err) : resolve(hash);
             });
         });
-    }
+    },
+
+    /**
+     * Validates a password against a bcrypt hash
+     * @param password
+     * @param hash
+     */
+    validatePassword(password: string, hash: string) {
+        return new Promise<boolean>((resolve, reject) => {
+            bcrypt.compare(password, hash, (err, valid) => {
+                err ? reject(err) : resolve(valid);
+            });
+        });
+    },
 };
 
