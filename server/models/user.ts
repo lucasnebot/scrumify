d69b1977e43c05bccd00861b26917cf1ae7ed45c
Lucas Nebot <lnebothh@gmail.com>
2017-12-12 18:56:35 +0100

    Fix missing user interface

--- server/models/user.ts
+++ server/models/user.ts
@@ -1,28 +1,35 @@
-import { model, Schema} from 'mongoose';
+import { model, Schema } from 'mongoose';
+
+export interface User {
+  email: string;
+  name: string;
+  surname: string;
+  password: string;
+}
 
 export const userSchema = new Schema({
-    email: {
-        type: String,
-        required: true
-    },
-    name: {
-        type: String,
-        required: true
-    },
-    surname: {
-        type: String,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    role: {
-        type: Number,
-        required: true,
-        min: 0,
-        max: 2
-    }
+  email: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  surname: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  role: {
+    type: Number,
+    required: true,
+    min: 0,
+    max: 2
+  }
 });
 
 export const userModel = model('User', userSchema);
