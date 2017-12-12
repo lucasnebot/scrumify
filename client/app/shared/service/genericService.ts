dda16350208229422913117b8ce57b331318a6cc
Dennis Enselmann <d.enselmann@fh-muenster.de>
2017-12-12 19:03:21 +0100

    Fix generic service

--- client/app/shared/service/genericService.ts
+++ client/app/shared/service/genericService.ts
@@ -3,29 +3,28 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment';
 // Export
-export {HttpClient} from '@angular/common/http';
+export { HttpClient } from '@angular/common/http';
 
 export abstract class GenericService<T> {
-    // * Local: "http://localhost:3000/api" | Web: "https://scrumify.herokuapp.com/api"
     private BASE_URL: string = environment.api_uri;
 
     constructor(protected http: HttpClient, protected actionUrl: string) {}
 
     getAll(): Observable<T[]> {
-      return this.http.get(BASE_URL + this.actionUrl).map(resp => resp as T[]);
+      return this.http.get(this.BASE_URL + this.actionUrl).map(resp => resp as T[]);
     }
     // Changed id type to string and fix url
     getOne(id: string): Observable<T> {
-      return this.http.get(BASE_URL + `${this.actionUrl}/${id}`).map(resp => resp as T);
+      return this.http.get(this.BASE_URL + `${this.actionUrl}/${id}`).map(resp => resp as T);
     }
     add(document: T): Observable<any> {
-      return this.http.post(BASE_URL + this.actionUrl, document);
+      return this.http.post(this.BASE_URL + this.actionUrl, document);
     }
     delete(id: string): Observable<any> {
-      return this.http.delete(BASE_URL + `${this.actionUrl}/${id}`);
+      return this.http.delete(this.BASE_URL + `${this.actionUrl}/${id}`);
     }
     edit(id: string, update: Object): Observable<T> {
-      return this.http.put(BASE_URL + `${this.actionUrl}/${id}`, update).map(resp => resp as T);
+      return this.http.put(this.BASE_URL + `${this.actionUrl}/${id}`, update).map(resp => resp as T);
     }
   }
 
