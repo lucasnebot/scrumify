dde337ce9e13f6c9c6ba232d305d69819c6fbdb1
Dennis Enselmann <d.enselmann@fh-muenster.de>
2017-12-12 18:48:08 +0100

    Set REST API environment variables in angular app

--- client/app/shared/service/genericService.ts
+++ client/app/shared/service/genericService.ts
@@ -1,21 +1,21 @@
-import { Observable} from 'rxjs/Observable';
-import {HttpClient} from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment';
 // Export
 export {HttpClient} from '@angular/common/http';
 
-//* Local: "http://localhost:3000/api" | Web: "https://scrumify.herokuapp.com/api"
-const BASE_URL = "https://scrumify.herokuapp.com/api";
+// * Local: "http://localhost:3000/api" | Web: "https://scrumify.herokuapp.com/api"
+const BASE_URL = environment.api_uri;
 
 export abstract class GenericService<T> {
-    constructor(protected http: HttpClient, protected actionUrl:string){
-    }
-  
-    getAll():Observable<T[]> {
+    constructor(protected http: HttpClient, protected actionUrl: string) {}
+
+    getAll(): Observable<T[]> {
       return this.http.get(BASE_URL + this.actionUrl).map(resp => resp as T[]);
     }
     // Changed id type to string and fix url
-    getOne(id:string):Observable<T> {
+    getOne(id: string): Observable<T> {
       return this.http.get(BASE_URL + `${this.actionUrl}/${id}`).map(resp => resp as T);
     }
     add(document: T): Observable<any>{
@@ -24,7 +24,7 @@ export abstract class GenericService<T> {
     delete(id: string): Observable<any>{
       return this.http.delete(BASE_URL + `${this.actionUrl}/${id}`);
     }
-    edit(id: string, update: Object):Observable<T>{
+    edit(id: string, update: Object): Observable<T>{
       return this.http.put(BASE_URL + `${this.actionUrl}/${id}`, update).map(resp => resp as T);
     }
   }
--- client/environments/environment.prod.ts
+++ client/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  api_uri: 'https://scrumify.herokuapp.com/api'
 };
