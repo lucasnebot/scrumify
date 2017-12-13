b32888525a3365a2c1ceede50e07194ff7f3ef4f
Christian Pleister <cp868013@fh-muenster.de>
2017-12-13 15:39:17 +0100

    Add new bli

--- client/app/backlog/backlog.component.ts
+++ client/app/backlog/backlog.component.ts
@@ -1,8 +1,7 @@
+import { UserStory } from './../shared/model/userStory';
 import { BacklogItem } from './../shared/model/backlogItem';
 import { BacklogService } from './../shared/service/backlog.service';
 import { Component, OnInit } from '@angular/core';
-import { UserStory } from '../shared/model/userStory';
-
 import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'app-backlog',
@@ -11,8 +10,9 @@ import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 })
 export class BacklogComponent implements OnInit {
   backlogItems: BacklogItem[];
-  selectedItem: BacklogItem = {title: '', description: '', order: 0}; 
+  newBacklogItem: UserStory = {title: '', description: '', order: 0, estimation:0,status:'NEW',task:[]};
   selectedIndex;
+  modal;
   constructor(private backlogService: BacklogService, private modalService: NgbModal){
   }
 
@@ -39,18 +39,22 @@ console.log('updateOrder called')
   open(content,index) {
     this.selectedIndex = index;
     console.log(this.selectedIndex);
-    this.modalService.open(content).result.then((result) => {
+    this.modal = this.modalService.open(content);
+    this.modal.result.then((result) => {
 
     }, (reason) => {
     });
+    
   }
 
   isUserStory(){
   return  this.backlogItems[this.selectedIndex].hasOwnProperty('type'); 
 }
+
   saveItem(index){
     console.log('Save Item');
     this.backlogService.edit(this.backlogItems[index]._id,this.backlogItems[index]).subscribe((success) => {
+      this.modal.close();
     })
   }
 }
