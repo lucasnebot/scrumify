09258b08edb047a4353a0dc38996816c9c244099
Christian Pleister <cp868013@fh-muenster.de>
2017-12-20 19:30:14 +0100

    visuals

--- client/app/shared/service/backlog.service.ts
+++ client/app/shared/service/backlog.service.ts
@@ -12,4 +12,30 @@ export class BacklogService extends GenericService<BacklogItem>{
     updateOrder(update: Object[]): Observable<any> {
         return this.http.put(this.BASE_URL + `${this.actionUrl}`, update);
       }
+
+      getReadableStatus(status: string):string{
+          switch(status){
+              case "EPIC":
+              return "";
+              case "RFE":
+              return "Ready for Estimation";
+              case "RFS":
+              return "Ready for Sprint";
+              case "SPRINT":
+              return "Sprint";
+              case "DONE":
+              return "Done";
+              default:
+              return status;
+          }
+      }
+
+      getType(bli:BacklogItem): string{
+        if(bli.status == "EPIC"){
+            return 'Epic';
+        }
+        else{
+            return 'User Story';
+        }
+      }
 }
