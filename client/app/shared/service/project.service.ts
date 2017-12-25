7ca0b52c0458257f8de4eea20f0fba24ee5bbd62
Lucas Nebot <lnebothh@gmail.com>
2017-12-25 20:31:50 +0100

    Add  saving of user id in project document on signing up

--- client/app/shared/service/project.service.ts
+++ client/app/shared/service/project.service.ts
@@ -1,25 +1,43 @@
 import { Injectable } from '@angular/core';
 import { Project } from '../model';
-import {GenericService, HttpClient} from './generic.service';
-
+import { GenericService, HttpClient } from './generic.service';
+import { UserService } from './user.service';
 
 @Injectable()
-export class ProjectService extends GenericService<Project>{
+export class ProjectService extends GenericService<Project> {
   project: Project;
+  numberOfDevelopers: number;
 
   // Fetches the current project once on construction
-  constructor(http: HttpClient) {
+  constructor(http: HttpClient, private userService: UserService) {
     super(http, '/project');
     this.fetchProject();
   }
-    fetchProject(){
-      this.getAll().subscribe((resp) => {
-        if(resp.length >= 1) {
-          this.project = resp[0];
-        } else {
-          this.project = null;
-        }
-      })
-    }
-
+  fetchProject() {
+    this.getAll().subscribe(resp => {
+      if (resp.length >= 1) {
+        this.project = resp[0];
+        this.getNumberOfDevelopers();
+      } else {
+        this.project = null;
+      }
+    });
+  }
+  addProjectMember(userId: string) {
+   this.edit(
+      this.project._id,
+      {
+        $push: { users: userId }
+      },
+      true
+    ).subscribe(() => {
+      console.log('Project Member added');
+    });
+  }
+  // TODO : Extract a getProjectMembers() method
+  getNumberOfDevelopers() {
+    this.userService.getAll({_id: {$in: this.project.users}, role: 2}).subscribe((resp) => {
+      this.numberOfDevelopers = resp.length;
+    })
+  }
 }
