79a33831c5b758809d8776a88a91cdee635f7ca8
Lucas Nebot <lnebothh@gmail.com>
2018-01-10 18:00:32 +0100

    Make roadmap project specific

--- client/app/roadmap/roadmap.component.ts
+++ client/app/roadmap/roadmap.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Milestone } from '../shared/model';
-import { MilestoneService, AuthService } from '../shared/service';
+import {
+  MilestoneService,
+  AuthService,
+  ProjectService
+} from '../shared/service';
 
 @Component({
   selector: 'app-roadmap',
@@ -14,16 +18,20 @@ export class RoadmapComponent implements OnInit {
     title: '',
     description: '',
     date: '',
-    project: ''
+    project: this.projectService.project._id
   };
 
-  constructor(private milestoneService: MilestoneService, public authService: AuthService) {}
-// TODO : get only milestones of project
+  constructor(
+    private milestoneService: MilestoneService,
+    public authService: AuthService,
+    protected projectService: ProjectService
+  ) {}
+
   ngOnInit() {
-    this.milestoneService.getAll().subscribe((docs) => {
+    this.milestoneService.getAll({project: this.projectService.project._id}).subscribe(docs => {
       this.milestones = docs;
       this.sortMilestones();
-    })
+    });
   }
 
   toggleForm(): void {
@@ -32,7 +40,7 @@ export class RoadmapComponent implements OnInit {
       // Clear Form and hide it
       this.milestone.title = '';
       this.milestone.description = '';
-      this.milestone.date = ''; 
+      this.milestone.date = '';
     }
     this.formHidden = !this.formHidden;
   }
@@ -43,29 +51,28 @@ export class RoadmapComponent implements OnInit {
       description: this.milestone.description,
       date: this.milestone.date,
       project: this.milestone.project
-    }
-    this.milestoneService.add(milestone).subscribe((resp) => {
+    };
+    this.milestoneService.add(milestone).subscribe(resp => {
       this.ngOnInit();
-    })
+    });
   }
 
   deleteMilestone(id: string): void {
-    this.milestoneService.delete(id).subscribe((resp) => {
+    this.milestoneService.delete(id).subscribe(resp => {
       this.ngOnInit();
-    })
+    });
   }
 
-  achieveMilestone(id:string): void{
-    let update = {achieved: true};
-    this.milestoneService.edit(id, update).subscribe((resp) => {
+  achieveMilestone(id: string): void {
+    let update = { achieved: true };
+    this.milestoneService.edit(id, update).subscribe(resp => {
       this.ngOnInit();
-    })
+    });
   }
-  
+
   sortMilestones() {
     this.milestones.sort((a: Milestone, b: Milestone): number => {
       return Date.parse(a.date) - Date.parse(b.date);
-    })
+    });
   }
 }
-
