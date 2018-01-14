800465c14396bf4f4fa485c7bacfe804898ab31b
Christian Pleister <cp868013@fh-muenster.de>
2018-01-14 22:41:45 +0100

    multi project

--- client/app/scrumboard/scrumboard.component.ts
+++ client/app/scrumboard/scrumboard.component.ts
@@ -1,6 +1,7 @@
 import { BacklogItem, User } from './../shared/model/';
 import { BacklogService, UserService } from './../shared/service';
 import { Component, OnInit } from '@angular/core';
+import { ProjectService } from '../shared/service/project.service';
 
 @Component({
   selector: 'app-scrumboard',
@@ -8,20 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./scrumboard.component.css']
 })
 export class ScrumboardComponent implements OnInit {
-
-  constructor(private backlogService: BacklogService,
-  private userService: UserService) { }
-  backlogItems: BacklogItem[] =[];
-  taskStates: string[] = ['Todo','Doing','Review','Done'];
-  developers: User[]= [];
+  constructor(
+    private backlogService: BacklogService,
+    private userService: UserService,
+    private projectService: ProjectService
+  ) {}
+  backlogItems: BacklogItem[] = [];
+  taskStates: string[] = ['Todo', 'Doing', 'Review', 'Done'];
+  developers: User[] = [];
   ngOnInit() {
     this.backlogService.getAll({ status: 'SPRINT' }).subscribe(result => {
       this.backlogItems = result;
     });
 
-    this.userService.getAll({'role': 2}).subscribe((result)=>{
+    this.userService.getAll({ role: 2 , projects: this.projectService.project._id}).subscribe(result => {
       this.developers = result;
-    })
+    });
   }
-
 }
