75b0adae1ae7c164aca68ad257d28e5c576685c4
Christian Pleister <cp868013@fh-muenster.de>
2018-01-23 11:42:02 +0100

    now only bli's in active sprint are displayed

--- client/app/scrumboard/scrumboard.component.ts
+++ client/app/scrumboard/scrumboard.component.ts
@@ -1,3 +1,4 @@
+import { SprintService } from './../shared/service/sprint.service';
 import { BacklogItem, User } from './../shared/model/';
 import { BacklogService, UserService } from './../shared/service';
 import { Component, OnInit } from '@angular/core';
@@ -12,18 +13,25 @@ export class ScrumboardComponent implements OnInit {
   constructor(
     private backlogService: BacklogService,
     private userService: UserService,
-    private projectService: ProjectService
+    private projectService: ProjectService,
+    private sprintService: SprintService
   ) {}
   backlogItems: BacklogItem[] = [];
   taskStates: string[] = ['Todo', 'Doing', 'Review', 'Done'];
   developers: User[] = [];
+  sprintNo = 0;
   ngOnInit() {
-    this.backlogService.getAll({ status: 'SPRINT' }).subscribe(result => {
-      this.backlogItems = result;
-    });
 
-    this.userService.getAll({ role: 2 , projects: this.projectService.project._id}).subscribe(result => {
-      this.developers = result;
+    //get all bli's of active sprint
+    this.sprintService.getOne(this.projectService.project.activeSprint).subscribe(sprint =>{
+      this.sprintNo = sprint.sprintNo;
+      this.backlogService.getAll({_id: { $in: sprint.backlogItems }}).subscribe(blis =>{
+        this.backlogItems = blis;
+      })
+    })
+
+    this.userService.getAll({ role: 2 , projects: this.projectService.project._id}).subscribe(devs => {
+      this.developers = devs;
     });
   }
 }
